c00a69d107294a2a2868a7ec1592801006c4f6a4
abcd0978 <kimmin36944@gmail.com>
2021-08-10 02:29:54 +0900

    container에 lnb만듬

--- routes/pages.js
+++ routes/pages.js
@@ -11,7 +11,7 @@ router.get('/register',(req,res)=>//회원가입 페이지
 {
     res.sendFile(path.join(__dirname,'../views','register.html'));
 })
-router.route('/login').get((req,res,next)=>
+router.route('/login').get((req,res,next)=>//로그인 페이지
 {
     res.sendFile(path.join(__dirname,'../views','login.html'));
 })
@@ -20,6 +20,22 @@ router.route('/chatting').get((req,res,next)=>//채팅 페이지
     res.sendFile(path.join(__dirname,'../views','chatting.html'));
 })
 router.route('/mypage').get((req,res,next)=>//마이페이지 페이지
+{
+    res.sendFile(path.join(__dirname,'../views','mypage.html'));
+})
+router.route('/computersc').get((req,res,next)=>//컴퓨터사이언스 페이지
+{
+    res.sendFile(path.join(__dirname,'../views','mypage.html'));
+})
+router.route('/sfw').get((req,res,next)=>//SFW 페이지
+{
+    res.sendFile(path.join(__dirname,'../views','mypage.html'));
+})
+router.route('/nsfw').get((req,res,next)=>//nsfw 페이지
+{
+    res.sendFile(path.join(__dirname,'../views','mypage.html'));
+})
+router.route('/algorithm').get((req,res,next)=>//알고리즘 페이지
 {
     res.sendFile(path.join(__dirname,'../views','mypage.html'));
 })
